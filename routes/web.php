0b33627b36556a377ce899a1c245a1ec89969763
Laden <delfanladen24@gmail.com>
2023-08-06 21:51:34 +0800

    Create role middleware

--- routes/web.php
+++ routes/web.php
@@ -16,24 +16,27 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// ! Dapat diakses sebelum login
 Route::get('/', [HomeController::class, 'index'])->name('login');
 Route::post('/', [HomeController::class, 'login']);
 
-// Route::get('/')
+// ! Hanya dapat diakses ketika sudah melakukan login
 Route::group(['middleware' => ['auth']], function () {
     Route::get('/logout', [HomeController::class, 'logout']);
     Route::get('/home', [HomeController::class, 'home']);
     Route::get('/riwayat_monitoring', [HomeController::class, 'riwayat_monitoring']);
-    
-    // Fuzzy 
+    // Fuzzy (Uji coba perhitungan)
     Route::get('/fuzzy', [FuzzyController::class, 'index']);
+    // Setting profike
+    Route::get('/profile',[UserController::class,'profile']);
+    Route::post('/profile/updateProfile/{id}',[UserController::class,'updateProfile']);
+});
+// ! Hanya dapat diakses oleh admin
+Route::group(['middleware' => ['auth','role:admin']], function () {
     // Crud data user
     Route::get('/user',[UserController::class,'index']);
     Route::post('/user/store',[UserController::class,'store']);
     Route::post('/user/update/{id}',[UserController::class,'update']);
     Route::get('/user/delete/{id}',[UserController::class,'destroy']);
-
-    // Setting profike
-    Route::get('/profile',[UserController::class,'profile']);
-    Route::post('/profile/updateProfile/{id}',[UserController::class,'updateProfile']);
 });
+
